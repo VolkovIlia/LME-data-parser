3e65b764d9aa7defce94e5d3d13cff18b9305c10
Ilia Volk <ilia.volkov@outlook.com>
2022-09-03 02:24:39 +0300

    Finished alpha

--- src/lme.py
+++ src/lme.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
 from selenium_stealth import stealth
 import time
+from datetime import datetime
 from selenium.webdriver.common.by import By
+import pandas as pd
+import numpy as np
 
 options = webdriver.ChromeOptions()
 options.add_argument("start-maximized")
@@ -28,13 +31,22 @@ names = block.find_element(By.TAG_NAME, "ul")
 metals = names.find_elements(By.TAG_NAME, "li")
 urls = []
 names = []
-price = []
+bids = []
+offers = []
 for metal in metals:
     urls.append(metal.find_element(By.TAG_NAME, "a").get_attribute("href"))
-time.sleep(5)
+time.sleep(1)
 for i in urls:
-    driver.get(i)
-    time.sleep(2)
-    # table = driver.find_element(By.
-
+    try:
+        driver.get(i)
+        time.sleep(1)
+        names.append(driver.find_element(By.CLASS_NAME, "data-set-table__title").text)
+        bids.append(driver.find_element(By.CLASS_NAME, "data-set-table__body").find_element(By.XPATH, "//*[@id=\"dataset-tab-0\"]/div/div[2]/div[2]/div[1]/div/div[1]/table/tbody/tr[1]/td[1]").text)
+        offers.append(driver.find_element(By.CLASS_NAME, "data-set-table__body").find_element(By.XPATH, "//*[@id=\"dataset-tab-0\"]/div/div[2]/div[2]/div[1]/div/div[1]/table/tbody/tr[1]/td[2]").text)
+    except:
+        pass
 driver.quit()
+d = dict({"names" : names, "bids" : bids, "offers" : offers})
+df = pd.DataFrame(dict([ (k,pd.Series(v)) for k,v in d.items() ]))
+file_name = str(datetime.now().date())+".csv"
+df.to_csv(file_name, index=False)
